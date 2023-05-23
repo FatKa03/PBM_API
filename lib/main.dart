617473d8dc5beea6dcb7d5b3d60e0c3bb99138c6
unknown <fatikhamila@gmail.com>
2023-05-24 01:06:12 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/home.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'FlutterHttp',
+      theme: ThemeData(appBarTheme: AppBarTheme(elevation: 0)),
+      home: Home(),
+    );
+  }
+}
